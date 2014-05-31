9841f03513f674c7f9d2178678cb58bdf92b04dc
Andrey Lebedev <andrey@lebedev.lt>
2014-06-01 00:51:15 +0300

    More performance for popover autohider

--- src/shoobx/flowview/flowview.js
+++ src/shoobx/flowview/flowview.js
@@ -125,6 +125,7 @@ function initPopover(e, indexedTags) {
         container: "body"
     }).click(function() {
         var contents = makeTagPopoverHtml(taginfo);
+        $(this).addClass("tag-active");
         $(this).attr("data-content", "<ul class='tags'>" + contents + "</ul>");
         $(this).popover('toggle');
     });
@@ -167,11 +168,12 @@ $(function() {
     setupBaseUrl();
 
     $('body').on('click', function (e) {
-        $('.tag').each(function () {
+        $('.tag-active').each(function () {
 
             //the 'is' for buttons that trigger popups
             //the 'has' for icons within a button that triggers a popup
             if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                $(this).removeClass("tag-active");
                 $(this).popover('hide');
             }
         });
