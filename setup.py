f6e062b148139eab1e87cd6ae263739a1097c489
Andrey Lebedev <andrey@lebedev.lt>
2014-06-19 22:38:45 +0300

    New version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_file(filename):
 
 setup(
     name="shoobx.flowview",
-    version='0.9.1',
+    version='0.9.2',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="XPDL Viewer and Browser",
