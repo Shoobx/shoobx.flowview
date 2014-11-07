dd6a8aaf113febe779ae8053d8805208f2307ec2
Andrey Lebedev <andrey@lebedev.lt>
2014-11-07 11:33:44 +0200

    Version increment

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_file(filename):
 
 setup(
     name="shoobx.flowview",
-    version='0.9.3',
+    version='0.9.4',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="XPDL Viewer and Browser",
