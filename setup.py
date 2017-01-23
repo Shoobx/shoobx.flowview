65a0f046530dd0f5e3b0e3c3c2ed458565311894
Stephan Richter <stephan.richter@gmail.com>
2017-01-23 08:57:39 -0500

    Back to development: 0.9.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_file(filename):
 
 setup(
     name="shoobx.flowview",
-    version='0.9.5',
+    version='0.9.6.dev0',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="XPDL Viewer and Browser",
